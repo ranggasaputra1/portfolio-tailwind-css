1bb259078d1a80417a0c31ecc6ac46d043e97331
ranggasaputra1 <ranggasaputra10092003@gmail.com>
2024-04-11 13:31:11 +0700

    create dark mode with tailwind css done

--- dist/js/script.js
+++ dist/js/script.js
@@ -33,13 +33,26 @@ window.addEventListener("click", function (e) {
 });
 
 //Dark Mode Toggle
-const darkToggle = document.querySelector("#dark-Toggle");
+const darkToggle = document.querySelector("#dark-toggle");
 const html = document.querySelector("html");
 
 darkToggle.addEventListener("click", function () {
-  if (darkToggle.chechked) {
+  if (darkToggle.checked) {
     html.classList.add("dark");
+    localStorage.theme = "dark";
   } else {
     html.classList.remove("dark");
+    localStorage.theme = "light";
   }
 });
+
+//Pindahkan Posisi Toggle Sesuai Mode
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
